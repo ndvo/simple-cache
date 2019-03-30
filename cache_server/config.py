e851a8e64f895806db2c76b397032863364cf4db
ndvo <nelson@ocastudios.com>
2019-03-30 09:06:57 -0300

    Incomplete: began working on geo location

--- cache_server/config.py
+++ cache_server/config.py
@@ -1,10 +1,10 @@
 
 
-
+# cache.id will be automatically generated
 cache = type('cache', (), {})()
 cache.host = '0.0.0.0'
 cache.port = 3000
-cache.role = 'main'
+cache.role = 'secondary'
 cache.folder = '/dev/shm/simplecache'
 # set the cache.maxtime in seconds
 # seconds * minutes * hours
@@ -14,6 +14,9 @@ server = type('server', (), {})()
 server.host =  'localhost'
 server.port = 3001
 
+geoip = type('geoip', (), {})()
+geoip.path = "resources/GeoLite2-Country-CSV_20190326/GeoLite2-Country-Blocks-IPv4.csv"
+geoip.countries_path = "resources/GeoLite2-Country-CSV_20190326/GeoLite2-Country-Locations-en.csv"
 
 
-
+fakeip = "2.18.67.32"
--- cache_server/main.py
+++ cache_server/main.py
@@ -1,19 +1,43 @@
 import os
 import time
 import socket
+import ipaddress
+import base64
+
 from urllib.request import Request, urlopen, HTTPError
 
 """
     Cache proxy
 
-    This 
-    https://alexanderell.is/posts/simple-cache-server-in-python/
+    Original concept from: https://alexanderell.is/posts/simple-cache-server-in-python/
 """
 
 import config
 
+
+def setup():
+    """ Create an unique id for this server and persist it."""
+    print("... Set up ")
+    try:
+        id = open('resources/id', 'r').readline()[:-1]
+    except (FileNotFoundError, IndexError):
+        id = base64.b64encode(os.urandom(32))
+        with open('resources/id', 'wb') as idfile:
+            idfile.write(id)
+    config.cache.id = id
+    print("   cache server: "+str(id))
+    config.cache.ip = get_ip()
+    print("   cache ip = "+str(config.cache.ip))
+    config.cache.country, config.cache.countrycode= ip2country(config.cache.ip)
+    print("country ", config.cache.country or "country empty", config.cache.countrycode or "countrycode empty")
+
+
+
 # Index will keep track of the updated time of each cached file
 index = {}
+known_indexes = []
+
+servers = {}
 
 # Create an in memory storage in order to take advantage of the tree structure and performance
 try:
@@ -26,32 +50,25 @@ def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((config.cache.host, int(config.cache.port)))
-
     s.listen(1)
-
     print('Cache proxy is listening on port %s ...' % config.cache.port)
-
     while True:
         # Wait for client connection
         client_connection, client_address = s.accept()
-        print("\n\n=====\nNew request:")
-
+        print("\n\n=====\nNew request from : "+str(client_address))
         # Get the client request
         request = client_connection.recv(1024).decode()
         print(request)
         if not request:
             continue
-
         # Parse HTTP headers
         headers = request.split('\n')
         top_header = headers[0].split()
         method = top_header[0]
         filename = top_header[1]
-
         # Index check
         if filename == '/':
             filename = '/index.html'
-
         # Get the file
         try:
             content = fetch_file(filename)
@@ -78,14 +95,12 @@ def main():
             except UnicodeDecodeError:
                 response = content
                 print("   ... binary content")
-
         else:
             response = b'HTTP/1.0 404 NOT FOUND\n\n File Not Found'
             print("  ... not found")
         # Send the response and close the connection
         client_connection.sendall(response)
         client_connection.close()
-
     # Close socket
     s.close()
 
@@ -151,7 +166,88 @@ def save_in_cache(filename, content):
     cached_file.write(content)
     cached_file.close()
     index[filename] = time.time()
-    
+
+
+def register_cache_server(request):
+    #TODO: register the cache servers
+    pass
+
+def share_data():
+    #TODO when new data is save, broadcast it to other servers
+    # Send hashed string of the index and the new data
+    pass
+
+def update_data():
+    #TODO: when new data is received from other servers
+    # check the hash, if different create a copy, add the new item and check the hash
+    # if match, save it
+    pass
+
+
+def announce_new_server():
+    #TODO: announce the new server to all others
+    # when a new server is registered, annouce it's presence to all others
+    my_ip = get_ip()
+    for k,v in servers.items():
+        s = socket.socket()
+        s.bind((v.address, v.port))
+        s.sendall(config.cache)
+
+
+
+    pass
+
+def replace_main_server():
+    #TODO: if the main server is down, replace it
+    #if 'main' not in servers:
+    # loop through all servers voting on the server with more votes to be accepted as main server
+    # log votes to be main server from other servers
+    # If there is a tie, vote for the first.
+    # If I am one of the tied, vote for me. 
+    pass
+
+def get_ip():
+    """Returns the IP address of the current instance
+    Public domain code from Jamieson Becker: https://stackoverflow.com/a/28950776/935645
+    """
+    if config.fakeip:
+        return config.fakeip
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+   
+def ip2country(ip):
+    iprange = ".".join(ip.split(".")[:-1])
+    countrycode = None
+    country = None
+    with open(config.geoip.path, "r") as countryblocks:
+        for line in countryblocks:
+            if ( line.startswith(iprange)):
+                line_data = line.split(',')
+                if ipaddress.ip_address(ip) in ipaddress.ip_network(line_data[0]):
+                    countrycode = line_data[2]
+    if countrycode:
+        with open(config.geoip.countries_path, "r") as countries:
+            for line in countries:
+                print(line, countrycode)
+                if ( line.startswith(countrycode)):
+                    line_data = line.split(',')
+                    if (countrycode == line_data[0]):
+                        country = line_data[4]
+    return country, countrycode
+
+
+        
+
+
 
 if __name__ == '__main__':
+    setup()
     main()
